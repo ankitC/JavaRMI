37311d658c52b920d40069b48489ad1a01a6677e
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-09-28 16:45:48 -0400

    Adding the handler to Invoke objectMethods remotely*/

---
+++ src/RemoteInvocationHandler.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.net.Socket;
+
+public class RemoteInvocationHandler implements InvocationHandler {
+
+    private String host;
+    private int port;
+    private String objectID;
+
+    public RemoteInvocationHandler(String host,int port, String objId){
+        this.host = host;
+        this.port = port;
+        this.objectID = objId;
+    }
+
+    @Override
+    public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+
+        RemoteInvocationMessage invokeMsg = new RemoteInvocationMessage(objectID,method.getName(),objects);
+
+        Socket sock = null;
+        Object returnObject = null;
+
+        try {
+            sock = new Socket(this.host,this.port);
+            /*Marshall the Invokation Message*/
+            ObjectOutputStream oos = new ObjectOutputStream(sock.getOutputStream());
+            oos.writeObject(invokeMsg);
+            
+            /*Wait for the return Message and unmarhsall it*/
+            ObjectInputStream ois = new ObjectInputStream(sock.getInputStream());
+            ReturnMessage returnedMessage = (ReturnMessage)ois.readObject();
+
+            if(!returnedMessage.isException()) { 
+            	returnObject = returnedMessage.getReturnValue();
+            } else {
+                throw (Exception) returnedMessage.getReturnValue();
+            }
+            
+            ois.close();
+            oos.close();
+            sock.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(sock != null) {
+                try {
+                    sock.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return returnObject;
+    }
+}
+
