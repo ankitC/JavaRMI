538c29208f30305235d7b07713b3ff218f1cda0a
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-09-28 14:41:27 -0400

    Adding a simple Exception Handler

---
+++ src/CustomRemoteException.java
@@ -0,0 +1,11 @@
+public class CustomRemoteException extends Exception {
+
+	public CustomRemoteException() {
+		super();
+	}
+
+	public CustomRemoteException(String message) {
+		super(message);
+	}
+
+}
