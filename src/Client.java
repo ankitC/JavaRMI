62759454243449f35a38b53773569ce901931ab9
Ankit Chheda <ankit.ac.chheda@gmail.com>
2013-10-08 16:54:49 -0400

    Adding WORKING client! HURRAYYgit add src/Client.java :D

---
+++ src/Client.java
@@ -0,0 +1,31 @@
+import registry.RemoteObjectReference;
+import serverDatabases.CapitalQueryInterface;
+import util.RegistryMessenger;
+
+
+public class Client {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		RegistryMessenger clientSideRegistryMessenger =  new RegistryMessenger();
+		try {
+			RemoteObjectReference localNationsDBObject = clientSideRegistryMessenger.lookup("CountriesDb");
+			System.out.println("Getting Stub");
+			CapitalQueryInterface countriesQuery = (CapitalQueryInterface)localNationsDBObject.getStub();
+			System.out.println("Got Stub");
+			RemoteObjectReference localStateDBObjectReference = clientSideRegistryMessenger.lookup("StatesDb");
+			CapitalQueryInterface statesQuery = (CapitalQueryInterface) localStateDBObjectReference.getStub();
+			
+			System.out.println(countriesQuery.getNationCapital("India").toString());
+			System.out.println(statesQuery.getStateCapital("Ohio").toString());
+		}
+			catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
